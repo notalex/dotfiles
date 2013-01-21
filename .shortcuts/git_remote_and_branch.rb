9930c20d71748c8d7a8539de9032bc7ee722e212
Alex J <alex.johnson@softwebsolutions.com>
2013-01-22 01:24:09 +0530

    refactoring git push/pull shortcuts

---
+++ .shortcuts/git_remote_and_branch.rb
@@ -0,0 +1,34 @@
+require 'optparse'
+
+def options
+  @options ||= ARGV.getopts('r:', 'f')
+end
+
+def options_string
+  '-f' if options['f']
+end
+
+def remote_name
+  remote_name = options['r'] || :origin
+end
+
+def remote_branch_name
+  branch = ARGV[0] || local_branch_name
+  case branch
+  when 'd'
+    "develop"
+  when 'r'
+    "release"
+  when 'm'
+    "master"
+  when 't'
+    "temp"
+  else
+    branch
+  end
+end
+
+def local_branch_name
+  branch_string = %x[ git branch ]
+  branch_string.slice(/\* (\w+)/, 1)
+end
