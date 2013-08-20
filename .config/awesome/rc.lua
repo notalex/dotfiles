61f9b4830052417fff44dff5a300bb5ca56d0bf5
notalex <notalexjohnson@gmail.com>
2013-08-20 16:21:53 +0530

    Using demu for awesome wm menu

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -244,7 +244,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
     -- Prompt
-    awful.key({ modkey },            "r",     function () mypromptbox[mouse.screen]:run() end),
+    awful.key({modkey }, "r", function() awful.util.spawn( "dmenu_run" ) end),
 
     awful.key({ modkey }, "x",
               function ()
