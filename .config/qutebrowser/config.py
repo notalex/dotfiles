8f9993696ee00c75dbc989a80a931f6615c296a1
Alex Johnson <notalexjohnson@gmail.com>
2018-12-02 20:44:23 +0530

    Refactor qutebrowser config

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,44 +5,45 @@ config.unbind('u')
 
 config.bind('dd', 'tab-close')
 config.bind('uu', 'undo')
-config.bind('t', 'set-cmd-text -s :open -t')
-config.bind('<ctrl-e>', 'scroll down')
-config.bind('<ctrl-y>', 'scroll up')
+config.bind('gT', 'tab-prev')
+config.bind('gt', 'tab-focus')
+config.bind('gl', 'tab-focus last')
 config.bind('sx', 'config-cycle statusbar.hide')
+config.bind('b', 'set-cmd-text -s :buffer')
+config.bind('t', 'set-cmd-text -s :open -t')
+config.bind('<ctrl-y>', 'scroll up')
+config.bind('<ctrl-e>', 'scroll down')
 config.bind('<Ctrl-n>', 'tab-next')
 config.bind('<Ctrl-p>', 'tab-prev')
-config.bind('gt', 'tab-focus')
-config.bind('gT', 'tab-prev')
-config.bind('gl', 'tab-focus last')
-config.bind('b', 'set-cmd-text -s :buffer')
 config.bind('<Ctrl-o>', 'set-cmd-text :open {url:pretty}')
 
+config.bind('<Ctrl-w>', 'rl-backward-kill-word', mode='command')
 config.bind('<Ctrl-i>', 'completion-item-focus next', mode='command')
 config.bind('<Ctrl-o>', 'completion-item-focus prev', mode='command')
-config.bind('<Ctrl-w>', 'rl-backward-kill-word', mode='command')
 
 config.bind('<Ctrl-s>', 'open-editor', mode='insert')
-config.bind('<Ctrl-h>', 'fake-key <Backspace>', mode='insert')
 config.bind('<Ctrl-i>', 'fake-key <Tab>', mode='insert')
 config.bind('<Ctrl-o>', 'fake-key <Shift-Tab>', mode='insert')
+config.bind('<Ctrl-h>', 'fake-key <Backspace>', mode='insert')
 config.bind('<Ctrl-m>', 'fake-key <Enter>', mode='insert')
 # Emacs in INSERT mode.
-config.bind('<Ctrl-a>', 'fake-key <Home>', mode='insert')
 config.bind('<Ctrl-e>', 'fake-key <End>', mode='insert')
-config.bind('<Ctrl-f>', 'fake-key <Right>', mode='insert')
+config.bind('<Ctrl-a>', 'fake-key <Home>', mode='insert')
 config.bind('<Ctrl-b>', 'fake-key <Left>', mode='insert')
-config.bind('<Ctrl-u>', 'fake-key <Shift+Home> ;; fake-key <Delete>', mode='insert')
+config.bind('<Ctrl-f>', 'fake-key <Right>', mode='insert')
 config.bind('<Ctrl-y>', 'insert-text {primary}', mode='insert')
+config.bind('<Ctrl-u>', 'fake-key <Shift+Home> ;; fake-key <Delete>', mode='insert')
 
-c.auto_save.session = True
 c.statusbar.hide = True
-c.tabs.background = True
-c.tabs.position = 'left'
-c.tabs.select_on_remove = 'last-used'
-c.tabs.width = '1%'
-c.tabs.indicator.width = 0
 c.scrolling.bar = 'never'
+c.auto_save.session = True
+
+c.tabs.width = '1%'
+c.tabs.position = 'left'
+c.tabs.indicator.width = 0
+c.tabs.background = True
 c.tabs.new_position.related = 'last'
+c.tabs.select_on_remove = 'last-used'
 
 c.aliases = {
     'cs': 'config-source',
